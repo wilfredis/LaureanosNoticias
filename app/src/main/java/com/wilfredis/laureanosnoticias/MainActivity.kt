d566fe914fef7f5ecaf5140025106a01a2cb5742
wilfredis <59529807+wilfredis@users.noreply.github.com>
2021-04-12 13:00:38 -0400

    menu

--- app/src/main/java/com/wilfredis/laureanosnoticias/MainActivity.kt
+++ app/src/main/java/com/wilfredis/laureanosnoticias/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
