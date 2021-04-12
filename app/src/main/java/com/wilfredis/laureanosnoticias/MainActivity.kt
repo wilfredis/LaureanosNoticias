9e40a37b7c015841ca245bd7f4c1592a0988b299
wilfredis <59529807+wilfredis@users.noreply.github.com>
2021-04-12 14:33:43 -0400

    add

--- app/src/main/java/com/wilfredis/laureanosnoticias/MainActivity.kt
+++ app/src/main/java/com/wilfredis/laureanosnoticias/MainActivity.kt
@@ -3,12 +3,39 @@ package com.wilfredis.laureanosnoticias
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.TextView
+import network.NotiApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textData: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textData = findViewById(R.id.txtData)
+
+        getnoti()
+    }
+
+    private fun getnoti(){
+
+        NotiApi.restrofitServices.getProperties().enqueue(
+                object : Callback<String>{
+                    override fun onResponse(call: Call<String>, response: Response<String>) {
+                       textData.text = response.body()
+                    }
+
+                    override fun onFailure(call: Call<String>, t: Throwable) {
+                        textData.text = "Err: " + t.message
+                    }
+                }
+        )
+      textData.text = "Respuesta"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
